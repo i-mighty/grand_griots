7cd08f1169e5a79992a3a38145268ddff1745864
josadegboye@gmail.com <i-mighty>
2019-08-05 00:53:50 +0100

    Added intro view

---
+++ app/views/Intro.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import AppIntroSlider from 'react-native-app-intro-slider';
+import firebase from 'react-native-firebase';
+
+class Intro extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    componentWillMount(){
+        if (firebase.auth().currentUser.uid) {
+            this.props.navigation.navigate('Auth')
+        } else {
+            
+        }
+    }
+
+    onSkipBtnHandle = (index) => {
+        Alert.alert('Skip');
+        console.log(index);
+    }
+    doneBtnHandle = () => {
+        Alert.alert('Done');
+    }
+    nextBtnHandle = (index) => {
+        Alert.alert('Next');
+        console.log(index);
+    }
+    onSlideChangeHandle = (index, total) => {
+        console.log(index, total);
+    }
+
+    pageArray = [{
+        title: 'Page 2',
+        description: 'Description 2',
+        image: require('../assets/slide1.jpg'),
+        imageStyle: {
+            height: 931,
+            width: 557,
+            resizeMode: 'center'
+        },
+        backgroundColor: '#EAC902',
+        fontColor: '#fff',
+        level: 10,
+    }, {
+        title: 'Page 1',
+        description: 'Description 1',
+        image: require('../assets/slide2.jpg'),
+        imageStyle: {
+            height: 931,
+            width: 557,
+            resizeMode: 'center'
+        },
+        backgroundColor: '#1C1B21',
+        fontColor: '#fff',
+        level: 15,
+    }];
+
+    render() {
+        return (
+            <AppIntroSlider
+                slides={this.pageArray}
+            />
+        );
+    }
+}
+
+export default Intro;
