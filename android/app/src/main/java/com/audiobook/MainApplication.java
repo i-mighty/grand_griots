b223ce8c03fdcb81f2f6ab473a5ac16861e2257d
josadegboye@gmail.com <i-mighty>
2019-08-06 17:48:07 +0100

    Added Firebase storage

--- android/app/src/main/java/com/audiobook/MainApplication.java
+++ android/app/src/main/java/com/audiobook/MainApplication.java
@@ -3,12 +3,14 @@ package com.audiobook;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.reactnativecommunity.slider.ReactSliderPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
 //import io.invertase.firebase.RNFirebaseAdMobPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage; // <-- Add this line
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage; // <-- Add this line
+import io.invertase.firebase.storage.RNFirebaseStoragePackage; // <-- Add this line
 import com.guichaguri.trackplayer.TrackPlayer;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.ijzerenhein.magicmove.ReactMagicMovePackage;
@@ -32,11 +34,13 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+            new ImagePickerPackage(),
             new ReactSliderPackage(),
             new LottiePackage(),
         new RNFirebasePackage(),
         new RNFirebaseAuthPackage(),
         new RNFirebaseFirestorePackage(), // <-- Add this line
+        new RNFirebaseStoragePackage(), // <-- Add this line
 //        new RNFirebaseAdMobPackage(),
         new TrackPlayer(),
         new RNGestureHandlerPackage(),
